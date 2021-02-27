88211272cf9981c5486278823a394aebc245cd3a
Riru <58894348+AugustoXDBR@users.noreply.github.com>
2021-02-27 19:56:02 -0300

    Correção da sanitização do input
    Primeiro passo para portar todos os comandos "não simples" do bot.

--- index.py
+++ index.py
@@ -2,19 +2,22 @@ import os
 import discord
 from dotenv import load_dotenv
 import json
-#importar token de config.json, mas agora uso .env
+import random
+from dict import comandos
+# importar token de config.json, mas agora uso .env
 
 data = open('links.json', "r")
 links = json.load(data)
-
+# importando o .env, para utilizar o token do bot e a lista de comandos simples
 load_dotenv()
 btoken = os.getenv('token')
 prefix = os.getenv('prefix')
 response_object = links
-
+# pemitindo o bot ver outras pessoas, e mais algumas coisas da API que eu com certeza entendo
 intents = discord.Intents.all()
 intents.members = True
 client = discord.Client(intents=intents)
+
 
 
 @client.event
@@ -29,16 +32,31 @@ async def on_message(message):
     if message.author.bot:
         return
 
+    # pequena função anônima para encurtar a mesma função de sempre
+    manda = lambda mens: message.channel.send('{}'.format(mens))
+
     if message.content.startswith(prefix):
         args = message.content[len(prefix):]
         args2 = args.strip().split()
-        comando = str(args2[1:]).lower()
+        argumentoslist = str(args2[1:]).lower()
+        comandolist = str(args2[:1]).lower()
+        comando = "".join(str(x) for x in comandolist)[2:len(comandolist)-2]
+        argumentos = "".join(str(x) for x in argumentoslist)[2:len(argumentoslist)-2]
 
-        if comando == 'teste':
-            await message.channel.send('testado!')
+        if comando == 'ping':
+            await manda('pong!')
+
+        elif comando == 'batata':
+            await manda('quente!')
+
+        elif comando == 'roleta':
+            numero = random.randint(0, 1)
+            if numero == 0:
+                await manda('Morreu!')
+            else:
+                await manda('Sobreviveu!')
 
     if message.content in response_object:
         await message.channel.send(response_object[message.content])
-
 
 client.run(btoken)
